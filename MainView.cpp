165a36ad2dac5d7222d0763b831c2004f6689bed
Alexandre Deckner <alex@zappotek.com>
2011-07-15 12:42:47 +0200

    Reapropriate copyright, i'm the only author up until now.
    Copyright headers were initially copypasted from another project without much thought.
    Small style fixes.

--- MainView.cpp
+++ MainView.cpp
@@ -1,10 +1,9 @@
 /*
- * Copyright 2011, Haiku Inc. All rights reserved.
+ * Copyright 2011, Alexandre Deckner (alex@zappotek.com)
  * Distributed under the terms of the MIT License.
  *
- * Authors:
- * 		Alexandre Deckner <alex@zappotek.com>
  */
+
 
 #include "MainView.h"
 
@@ -16,6 +15,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <new>
+
 
 MainView::MainView(BRect frame)
 	:
@@ -41,8 +41,8 @@ MainView::~MainView()
 void
 MainView::AttachedToWindow()
 {
-	BView::AttachedToWindow();	
-	_LoadWave();	
+	BView::AttachedToWindow();
+	_LoadWave();
 	MakeFocus();
 }
 
@@ -53,14 +53,14 @@ MainView::Draw(BRect updateRect)
 	if (fWave == NULL || fDestCursor == 0)
 		return;
 
-	float x = 0.0f;	
+	float x = 0.0f;
 	float ycenter = fWaveFrame.Height() / 2.0f;
 	float yscale = fWaveFrame.Height() / 2.0f;
-	
+
 	if (fDownsamplingWidth > 10) {
 		for (int32 i = 0; i < (int32) fWaveFrame.Width(); i++) {
 			float y = fWave[i] * yscale;
-			
+
 			StrokeLine(BPoint(x, ycenter - y), BPoint(x, ycenter + y));
 			x += 1.0f;
 		}
@@ -70,10 +70,10 @@ MainView::Draw(BRect updateRect)
 		for (int32 i = 0; i < (int32) fWaveFrame.Width(); i++) {
 			float y = ycenter + fWave[i] * yscale;
 			StrokeLine(BPoint(x, y));
-			x += 1.0f;		
+			x += 1.0f;
 		}
 	}
-	
+
 }
 
 
@@ -86,14 +86,14 @@ MainView::FrameResized(float width, float height)
 void
 MainView::_LoadWave() {
 	bigtime_t startLoadWave = system_time();
-	
+
 	fDestCursor = 0;
 	fSourceCursor = 0;
 	fAverage = 0.0f;
 	fAverageCursor = 0;
 	fDownsamplingWidth = 0;
 	fDownsampleCount = 0;
-		
+
 	// instantiate a BMediaFile object, and make sure there was no error.
 	BEntry entry("./song.wav");
 	entry_ref ref;
@@ -121,9 +121,9 @@ MainView::_LoadWave() {
 			return;
 		}
 
-		if (format.type == B_MEDIA_RAW_AUDIO) {			
-			
-			// get the actual sourceWindow.left obtained by seeking		
+		if (format.type == B_MEDIA_RAW_AUDIO) {
+
+			// get the actual sourceWindow.left obtained by seeking
 			bigtime_t start = system_time();
 			int64 actualSeekFrame = fSourceWindow.left;
 			err = track->SeekToFrame(&actualSeekFrame);
@@ -133,37 +133,37 @@ MainView::_LoadWave() {
 			}
 			printf("BMediaTrack::SeekToFrame(%lli) seekedto=%lli in %llims\n", fSourceWindow.left, actualSeekFrame, (system_time() - start) / 1000);
 			fSourceWindow.left = actualSeekFrame;
-			
+
 			frame_range destWindow(0, (uint64) fWaveFrame.Width());
-			int64 totalSourceFrameCount = track->CountFrames();			
+			int64 totalSourceFrameCount = track->CountFrames();
 			fDownsamplingWidth = fSourceWindow.Width() / destWindow.Width();
-			
+
 			printf("Source window left=%lli right=%lli width=%lli (total=%lli)\n", fSourceWindow.left, fSourceWindow.right, fSourceWindow.Width(), totalSourceFrameCount);
 			printf("Dest window left=%lli right=%lli width=%lli\n", destWindow.left, destWindow.right, destWindow.Width());
 			printf("Downsampling width=%lli\n", fDownsamplingWidth);
-			
+
 			delete [] fWave;
 			fWave = NULL;
 			fWave = new(std::nothrow) float[fSourceWindow.Width() / fDownsamplingWidth]; // ATT: on en prend plus que la largeur de dest car downsampling width entier
-			char *buffer = new(std::nothrow) char[format.u.raw_audio.buffer_size];			
-			
+			char *buffer = new(std::nothrow) char[format.u.raw_audio.buffer_size];
+
 			int64 readFrameCount = 0;
 			media_header mediaHeader;
 			fSourceCursor = fSourceWindow.left;
 			for(int64 j = 0; j < fSourceWindow.Width(); j += readFrameCount) {
-							
+
 				err = track->ReadFrames(buffer, &readFrameCount, &mediaHeader);
 				if (err) {
 					printf("BMediaTrack::ReadFrames error -- %s\n", strerror(err));
 					delete [] buffer;
 					break;
 				}
-				
+
 				if (fSourceCursor + readFrameCount >= fSourceWindow.right) {
 					readFrameCount = fSourceWindow.right - fSourceCursor;
-					printf("yes readFrameCount = %lli\n", readFrameCount);					
+					printf("yes readFrameCount = %lli\n", readFrameCount);
 				}
-				
+
 				_ProcessAudio(buffer, &format, readFrameCount);
 			}
 			printf("Source cursor %li (read %li)\n", fSourceCursor, fSourceCursor - fSourceWindow.left);
@@ -175,19 +175,19 @@ MainView::_LoadWave() {
 		mediaFile->ReleaseTrack(track);
 	}
 	delete mediaFile;
-	
-	
-	printf("LoadWave in %lims\n", (system_time() - startLoadWave) / 1000 );   
+
+
+	printf("LoadWave in %lims\n", (system_time() - startLoadWave) / 1000 );
 }
 
 
 void
 MainView::_ProcessAudio(char* buffer, media_format* format, int64 frameCount)
-{		
+{
 	//printf("framecount %u\n", frameCount);
-	
 
-	
+
+
 	uint32 channelCount = 2;
 
 	for (int64 frame = 0; frame < frameCount; frame++) {
@@ -196,29 +196,29 @@ MainView::_ProcessAudio(char* buffer, media_format* format, int64 frameCount)
 			if (fDownsamplingWidth > 10)
 				fAverage += value * value; // attention x 2 et au carré
 			else
-				fAverage += value;		
-		}		
-		
+				fAverage += value;
+		}
+
 		if (fAverageCursor == fDownsamplingWidth - 1) {
 			fAverage /= (float) fAverageCursor * (float) channelCount;
 			if (fDownsamplingWidth > 10)
 				fAverage = sqrt(fAverage);
 			fWave[fDestCursor] = fAverage;
 			fDestCursor++;
-			
+
 			fAverageCursor = 0;
 			fAverage = 0;
-		} else {		
+		} else {
 			fAverageCursor++;
 		}
 		fSourceCursor++;
 	}
-	
-	
-	
-	
+
+
+
+
 	//printf("fWaveCursor %ui\n", fWaveCursor);
-	
+
 	//printf("average = %f\n", average);	;
 }
 
@@ -229,7 +229,7 @@ void MainView::KeyDown(const char *bytes, int32 numBytes)
    if ( numBytes >= 1 ) {
    		int64 width = fSourceWindow.Width();
       switch ( bytes[0] ) {
-      case B_UP_ARROW:      	 
+      case B_UP_ARROW:
          fSourceWindow.left += width / 10;
          fSourceWindow.right -= width / 10;
          _LoadWave();
--- MainView.h
+++ MainView.h
@@ -1,25 +1,27 @@
 /*
- * Copyright 2011, Haiku Inc. All rights reserved.
+ * Copyright 2011, Alexandre Deckner (alex@zappotek.com)
  * Distributed under the terms of the MIT License.
  *
- * Authors:
- * 		Alexandre Deckner <alex@zappotek.com>
  */
 #ifndef _MAINVIEW_H
 #define _MAINVIEW_H
 
+
 #include <View.h>
 #include <Rect.h>
 
+
 struct media_format;
+
 
 struct frame_range {
 	int64 left;
 	int64 right;
-	
-	frame_range(int64 _left, int64 _right) : left(_left), right(_right) {};	
+
+	frame_range(int64 _left, int64 _right) : left(_left), right(_right) {};
 	int64 Width() const { return right - left; };
 };
+
 
 class MainView : public BView {
 public:
@@ -29,7 +31,7 @@ public:
 	virtual void	AttachedToWindow();
 	virtual void	FrameResized(float width, float height);
 	virtual void	KeyDown(const char *bytes, int32 numBytes);
-	
+
 	void			_LoadWave();
 	void			_ProcessAudio(char* buffer, media_format* format, int64 frameCount);
 	float*			fWave;
@@ -38,10 +40,11 @@ public:
 	float			fAverage;
 	uint32			fAverageCursor;
 	uint32			fDownsamplingWidth;
-	uint32			fDownsampleCount;	
-	
+	uint32			fDownsampleCount;
+
 	BRect			fWaveFrame;
 	frame_range 	fSourceWindow;
 };
+
 
 #endif	// _MAINVIEW_H
